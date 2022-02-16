4c5aaaa279cf5a1d0801dc31c250cbd449b636c1
Tom Siatka <tom.siatka@laposte.net>
2022-02-16 13:32:45 +0100

    add default controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function ping()
+    {
+        return $this->json([
+            'message' => 'pong'
+        ]);
+    }
+}
