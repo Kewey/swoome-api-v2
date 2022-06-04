317bbffc580e12aee46bc42ff97c6a2e9c7d0bfa
Tom Siatka <tom.siatka@laposte.net>
2022-06-04 16:05:45 +0200

    add username to balance

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -77,7 +77,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[Assert\NotBlank(["groups" => ["create"]])]
     private $plainPassword;
 
-    #[Groups(["user:read", "user:write", "group:read", "expense:read"])]
+    #[Groups(["user:read", "user:write", "group:read", "expense:read", 'balance:read'])]
     #[ORM\Column(type: 'string', length: 255)]
     private $username;
 
