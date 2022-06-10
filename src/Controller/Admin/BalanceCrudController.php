0a8c03859bcc248fc1b4f7ba21d7cbf7181dc645
Tom Siatka <tom.siatka@laposte.net>
2022-06-10 02:05:06 +0200

    add field to crud balance

--- src/Controller/Admin/BalanceCrudController.php
+++ src/Controller/Admin/BalanceCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Balance;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 
 class BalanceCrudController extends AbstractCrudController
 {
@@ -12,14 +14,12 @@ class BalanceCrudController extends AbstractCrudController
         return Balance::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            MoneyField::new('price', 'Prix')->setCurrency('EUR')->setStoredAsCents(true)->setRequired(true),
+            AssociationField::new('balanceUser', 'Utilisateur'),
+            AssociationField::new('balanceGroup', 'Groupe'),
         ];
     }
-    */
 }
