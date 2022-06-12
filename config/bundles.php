e51718d31b0bd77fcfb24cae7790f192d8335230
Tom Siatka <tom.siatka@laposte.net>
2022-06-12 10:48:24 +0200

    add sentry

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
